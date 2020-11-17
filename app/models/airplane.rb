a63e45aa49c40074a2b8c6a1907b47c8ef0f996f
hazim0001 <hazim_alsayed@hotmail.com>
2020-11-17 12:12:55 -0600

    changs in the rating migration

--- app/models/airplane.rb
+++ app/models/airplane.rb
@@ -6,5 +6,5 @@ class Airplane < ApplicationRecord
   validates :capacity, presence: true, numericality: { only_integer: true }
   validates :model, presence: true
   validates :price, presence: true, numericality: true
-  validates :rating, presence: true, inclusion: { in: (0.0..5.0) }, numericality: true
+  validates :rating, inclusion: { in: (0.0..5.0) }, numericality: true
 end
--- db/migrate/20201117015302_create_ratings.rb
+++ db/migrate/20201117015302_create_ratings.rb
@@ -1,7 +1,7 @@
 class CreateRatings < ActiveRecord::Migration[6.0]
   def change
     create_table :ratings do |t|
-      t.integer :rating, defualt: 0
+      t.integer :rating
       t.references :airplane, null: false, foreign_key: true
       t.references :renter, references: :users, foreign_key: {to_table: :users}
 
