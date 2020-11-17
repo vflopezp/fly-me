c2ef6840c1d497cf8f2394c38c753febe251ec6e
hazim0001 <hazim_alsayed@hotmail.com>
2020-11-17 16:43:14 -0600

    modified the root page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
       root to: 'devise/sessions#new'
       # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    end
+  end
 
   resources :airplanes do
     resources :bookings, only: :create
